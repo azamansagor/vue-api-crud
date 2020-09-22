1db14dfd5182b95420299e578ddccae6ab172f2a
azamansagor <a.zamansagor@gmail.com>
2020-09-23 02:02:51 +0600

    CORS problem fixed and key data print successfull

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <template v-for="data in datas">
+      <div v-for="(info, key) in data">
+        {{ key}} ====== {{ info }} <br>
+      </div>
+    </template>
+
   </div>
 </template>
 
@@ -10,10 +16,15 @@ export default {
   props: {
     msg: String
   },
+  data(){
+    return{
+      datas: {}
+    }
+  },
   mounted() {
     axios.get(`${this.$API_URL}/get_form.php`)
-      .then(function (response) {
-        console.log(response.data)
+      .then( response =>  {
+        this.datas = response.data.data.fields;
       }).catch(error => {
         console.log(error)
       });
