9a79f586c6a56a2936a8ba014961cc896ef038bf
azamansagor <a.zamansagor@gmail.com>
2020-09-24 15:48:35 +0600

    dragging done

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,32 +2,56 @@
   <div>
     <h1>{{ msg }}</h1>
     <h3 v-if="loading">Loading</h3>
-    <table>
-      <tr v-bind:key="header.index" v-for="header in headers">
-        <th v-bind:key="column.index" v-for="column in header">{{ column.title }}</th>
-      </tr>
+    <table class="table table-bordered">
+      <thead>
+        <tr v-bind:key="header.index" v-for="header in headers">
+          <th></th>
+          <th v-bind:key="column.index" v-for="column in header">{{ column.title }}</th>
+        </tr>
+      </thead>
+
+      <draggable :list="rows" tag="tbody" @dragstart="onDragStart">
+        <tr :key="row.index" v-for="row in rows" class="table-row">
+          <td>
+            <img alt="Drag Image" src="../assets/drag.svg" height="20px;">
+          </td>
+          <td :key="single.index" v-for="single in result(row) ">{{ single }}</td>
+        </tr>
+      </draggable>
+
+      <!--
       <tr v-bind:key="row.index" v-for="row in rows">
         <td v-bind:key="single.index" v-for="single in result(row) ">{{ single }}</td>
       </tr>
+      -->
     </table>
 
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable';
+
 export default {
   name: 'Index',
   props: {
     msg: String
   },
+  components: {
+    draggable
+  },
   data(){
     return{
-      headers: {},
-      rows: {},
-      loading: true
+      headers: [{}],
+      rows: [{}],
+      loading: true,
     }
   },
   methods: {
+    onDragStart(event){
+      event.style.backgroundColor = "blue";
+      console.log(event);
+    },
     result(row) {
       let newRow = {};
       let rowKeys = Object.keys(row);
@@ -51,10 +75,7 @@ export default {
           .then( response =>  {
             this.headers = response.data.data.headers;
             this.rows = response.data.data.rows;
-
-            setTimeout( () => {
-              this.loading = false;
-            }, 5000)
+            this.loading = false;
           }).catch(error => {
             console.log(error)
           }
@@ -63,8 +84,8 @@ export default {
   },
 
   created() {
-    this.rows = "";
-    this.headers = "";
+    this.rows = [{}];
+    this.headers = [{}];
     this.getList();
   }
 }
@@ -72,18 +93,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-table {
-  border-collapse: collapse;
-  width: 100%;
-}
-th, td{
-  height: 50px;
-  text-align: center;
-}
-th, td{
-  padding: 15px;
-}
-th, td {
-  border: 1px solid #ddd;
+.table-row:hover{
+  background: #e4e4e4;
 }
 </style>
